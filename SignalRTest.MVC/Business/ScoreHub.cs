f129a1fd0245df623f46161af8ba756a378d1a8c
TMHERL <ericloew@hotmail.com>
2019-05-14 14:22:52 -0500

    base

---
+++ SignalRTest.MVC/Business/ScoreHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRTest.MVC.Business
+{
+    public class ScoreHub : Hub
+    {
+        public async Task SendScore(string message)
+        {
+            await Clients.All.SendAsync("broadcastScore", message);
+        }
+    }
+}
---
+++ SignalRTest.MVC/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalRTest.MVC.Business;
+using SignalRTest.MVC.Models;
+
+//https://docs.microsoft.com/en-us/aspnet/core/tutorials/signalr?view=aspnetcore-2.2&tabs=visual-studio
+
+namespace SignalRTest.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IHubContext<ScoreHub> hubContext;
+
+        public HomeController(IHubContext<ScoreHub> hubContext)
+        {
+            this.hubContext = hubContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        [HttpPost]
+        public async Task<bool> sendScore(int userId)  //Web api method.
+        {
+            await hubContext.Clients.All.SendAsync("broadcastScore", "Message from server");
+
+            return true;
+
+        }
+    }
+}
